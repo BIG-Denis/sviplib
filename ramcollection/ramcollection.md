c20d98c3a1d44868bfb9e9a97e0ec2cc4fbb98ea
BIG-Denis <lozejor12@gmail.com>
2025-12-28 14:32:12 +0300

    Feat[simple_rams]: Add simple dual-port ram

--- ramcollection/ramcollection.md
+++ ramcollection/ramcollection.md
@@ -63,3 +63,36 @@ This is a **read-first** memory. When writing, data from cycle before write is p
 If no `INIT_FILE` provided, no memory initialization will be completed.
 
 By default, hexadecimal memory file format is used, if `INIT_FILE_BIN` is set to 1, then `INIT_FILE` will be interprented as binary format.
+
+### RAM - simple, simple dual-port, 1 cycle
+
+Read-write memory with two synchronous ports, one of them read-only, second one is write-only. One cycle two write data, one cycle to read data. Can be initialized from memory file.
+
+Source located in `rtl/ram_s2ps1c.sv`.
+
+Instance:
+
+```systemverilog
+ram_s1p1c #(
+  .WORD_WIDTH    (8  ),
+  .WORD_COUNT    (256),
+  .INIT_FILE_BIN (0  ),
+  .INIT_FILE     ("" ),
+) ram_instance_name (
+  .clk_i   (clk   ),
+  .rstn_i  (rstn  ),
+
+  .we_a_i  (we    ),
+  .addr_a_i(addr_a),  // ADDR_WIDTH = $clog2(WORD_COUNT)
+  .data_a_i(data_a),
+
+  .addr_b_i(addr_b),  // ADDR_WIDTH = $clog2(WORD_COUNT)
+  .data_b_o(data_b)
+);
+```
+
+This is a **read-first** memory. When writing, data from cycle before write is presented at read port.
+
+If no `INIT_FILE` provided, no memory initialization will be completed.
+
+By default, hexadecimal memory file format is used, if `INIT_FILE_BIN` is set to 1, then `INIT_FILE` will be interprented as binary format.
---
+++ ramcollection/rtl/ram_s2ps1c.sv
@@ -0,0 +1,51 @@
+module ram_s1p1c #(
+  parameter  int unsigned WORD_WIDTH      = 8,
+  parameter  int unsigned WORD_COUNT      = 256,
+  parameter  bit          INIT_FILE_BIN   = 0,
+  parameter  int unsigned INIT_FILE       = "",
+  localparam int unsigned ADDR_WIDTH      = $clog2(WORD_COUNT)
+) (
+  input  logic                    clk_i,
+  input  logic                    rstn_i,
+
+  input  logic                    we_a_i,
+  input  logic [ADDR_WIDTH - 1:0] addr_a_i,
+  input  logic [WORD_WIDTH - 1:0] data_a_i,
+
+  input  logic [ADDR_WIDTH - 1:0] addr_b_i,
+  output logic [WORD_WIDTH - 1:0] data_b_o
+);
+
+logic [WORD_WIDTH - 1:0] ram [0:WORD_COUNT - 1];
+
+
+// write port (a) driver
+always_ff @( posedge clk ) begin
+  if (we_a_i) begin
+    ram[addr_a_i] <= data_a_i;
+  end
+end
+
+// read port (b) driver
+always_ff @( posedge clk_i ) begin
+  data_b_o <= ram[addr_b_i];
+end
+
+
+initial begin
+  if (INIT_FILE != "") begin
+    if (INIT_FILE_BIN) begin
+      $display("Initializing RAM with bin file: %s...", INIT_FILE);
+      $readmemb(INIT_FILE, ram);
+    end
+    else begin
+      $display("Initializing RAM with hex file: %s...", INIT_FILE);
+      $readmemh(INIT_FILE, ram);
+    end
+  end
+end
+
+assert property (@(posedge clk) disable iff (!rstn_i) !$isunknown(we_a_i)) else $error("we_a_i must always be known");
+assert property (@(posedge clk) disable iff (!rstn_i) we_i |-> !$isunknown(addr_i)) else $error("addr_a_i must be known when we_a_i is high");
+
+endmodule

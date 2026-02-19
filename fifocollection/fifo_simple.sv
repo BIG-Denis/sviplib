dd056f39d6f4e50df59fcbe1ff0bff7292105076
SinDrag0n <vladislavbelyaev04@gmail.com>
2026-02-20 00:00:22 +0200

    Fix[fifos]: Fixed sync read mode

--- fifocollection/fifo_simple.sv
+++ fifocollection/fifo_simple.sv
@@ -11,45 +11,68 @@ module fifo_simple #(
   input  logic                         push_i,
 
   input  logic                         pop_i,
-  output logic [FIFO_DATA_WIDTH - 1:0] data_o
+  output logic [FIFO_DATA_WIDTH - 1:0] data_o,
+
+  output logic                         full_o,
+  output logic                         empty_o
 );
 
 localparam PTR_WIDTH = $clog2( WORD_FIFO_DEPTH );
+localparam MAX_PTR   = WORD_FIFO_DEPTH - 1;
 
 ////   LOCAL VARIABLES   ////
 
-logic [PTR_WIDTH:0] write_ptr;
-logic [PTR_WIDTH:0] read_ptr;
+logic [PTR_WIDTH - 1:0] write_ptr;
+logic [PTR_WIDTH - 1:0] read_ptr;
+logic [PTR_WIDTH - 1:0] read_ptr_next;
 logic write_ptr_circle, read_ptr_circle;
 
-logic [FIFO_DATA_WIDTH - 1:0] fifo_mem [WORD_FIFO_DEPTH - 1:0];
+logic [FIFO_DATA_WIDTH - 1:0] fifo_mem [0: WORD_FIFO_DEPTH - 1];
 
 
 ////     INNER LOGIC     ////
 
-assign write_ptr_circle = write_ptr[PTR_WIDTH];
-assign read_ptr_circle  =  read_ptr[PTR_WIDTH];
+// assign write_ptr_circle = write_ptr[PTR_WIDTH];
+// assign read_ptr_circle  =  read_ptr[PTR_WIDTH];
+
+assign read_ptr_next = (read_ptr < MAX_PTR ) ? ( read_ptr + pop_i ) : ( 1'b0 );
 
 always_ff @( posedge clk_i or negedge rstn_i ) begin : write_ptr_logic
   if ( ~rstn_i ) begin
-    write_ptr <= PTR_WIDTH'('b0);
+    write_ptr        <= PTR_WIDTH'('b0);
+    write_ptr_circle <= 1'b0;
   end
   else begin
     if ( flush_i ) write_ptr <= PTR_WIDTH'('b0);
     else if ( push_i ) begin
-      fifo_mem[write_ptr] <= data_i;
-      write_ptr           <= write_ptr + 1'b1;
+      fifo_mem[write_ptr]    <= data_i;
+      if ( write_ptr < MAX_PTR ) begin
+        write_ptr            <= write_ptr + 1'b1;
+      end
+      else begin
+        write_ptr            <= PTR_WIDTH'('b0);
+        write_ptr_circle     <= ~write_ptr_circle;
+      end
     end
   end
 end
 
 always_ff @( posedge clk_i or negedge rstn_i ) begin : read_ptr_logic
   if ( ~rstn_i ) begin
-    read_ptr <= PTR_WIDTH'('b0);
+    read_ptr        <= PTR_WIDTH'('b0);
+    read_ptr_circle <= 1'b0;
   end
   else begin
-    if    ( flush_i ) write_ptr <= PTR_WIDTH'('b0);
-    else if ( pop_i ) read_ptr  <= read_ptr + 1'b1;
+    if    ( flush_i ) read_ptr <= PTR_WIDTH'('b0);
+    else if ( pop_i ) begin
+      if ( read_ptr < MAX_PTR ) begin
+        read_ptr               <= read_ptr_next;
+      end
+      else begin
+        read_ptr               <= PTR_WIDTH'('b0);
+        read_ptr_circle        <= ~read_ptr_circle;
+      end
+    end
   end
 end
 
@@ -57,9 +80,9 @@ end
 
 generate
   if ( EGRESS ) begin
-    always_ff @( posedge clk_i ) data_o <= fifo_mem[read_ptr];
+    always_ff @( posedge clk_i ) data_o <= fifo_mem[read_ptr_next];
   end
-  else assign                    data_o  = fifo_mem[read_ptr];
+  else assign                    data_o  = fifo_mem[read_ptr_next];
 endgenerate
 
 
